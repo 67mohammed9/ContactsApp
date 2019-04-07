8b49ce7ec1154f86cd68f0f3e90fc04faf623c8a
67mohammed9 <mbhuiya002@citymail.cuny.edu>
2019-04-06 21:50:10 -0400

    Added buttons and fx:id

--- src/sample/ContactsAppController.java
+++ src/sample/ContactsAppController.java
@@ -1,11 +1,51 @@
 package sample;
 
-import javafx.fxml.Initializable;
-//test comment
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
-public class ContactsAppController  {
+import java.awt.event.ActionEvent;
 
+public class ContactsAppController {
 
+    @FXML
+    private ListView<Contacts> contactsListView;
 
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private TextField phoneNumberTextField;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private Button onAddButtonPressed;
+
+    @FXML
+    void onDeleteButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEditButtonPressed(ActionEvent event) {
+
+    }
+
+    public void onEditButtonPressed(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void onDeleteButtonPressed(javafx.event.ActionEvent actionEvent) {
+    }
 }
-
