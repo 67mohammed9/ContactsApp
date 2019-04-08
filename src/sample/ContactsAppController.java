3cacd93199fae05f59558c23c727fdea52870081
67mohammed9 <mbhuiya002@citymail.cuny.edu>
2019-04-08 13:32:55 -0400

    added save button

--- src/sample/ContactsAppController.java
+++ src/sample/ContactsAppController.java
@@ -8,11 +8,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
 import java.awt.event.ActionEvent;
 import java.util.Collections;
 import java.util.Comparator;
 
+
 public class ContactsAppController {
+    @FXML
+    private GridPane gridPane;
     @FXML
     private ListView<Contacts> contactsListView;
     @FXML
@@ -27,6 +32,9 @@ public class ContactsAppController {
     private Button deleteButton;
     @FXML
     private Button addButton;
+    @FXML
+    private Button saveButton;
+
     @FXML
     private TextField emailTextField;
 
@@ -78,13 +86,13 @@ public class ContactsAppController {
         final int selectedIdx = contactsListView.getSelectionModel().getSelectedIndex();
         if(selectedIdx != -1)
         {
-            Contacts changedinfo = contactsListView.getSelectionModel().getSelectedItem();
-            changedinfo.setFirst(firstNameTextField.getText());
-            changedinfo.setLast(lastNameTextField.getText());
-            changedinfo.setEmail(emailTextField.getText());
-            changedinfo.setPhoneNumber(phoneNumberTextField.getText());
+            Contacts editsMade = contactsListView.getSelectionModel().getSelectedItem();
+            editsMade.setFirst(firstNameTextField.getText());
+            editsMade.setLast(lastNameTextField.getText());
+            editsMade.setEmail(emailTextField.getText());
+            editsMade.setPhoneNumber(phoneNumberTextField.getText());
             contacts.remove(selectedIdx);
-            contacts.add(changedinfo);
+            contacts.add(editsMade);
             sortByLast lastNameSort = new sortByLast();
             Collections.sort(contacts, lastNameSort);
             contactsListView.setItems(contacts);
@@ -112,8 +120,26 @@ public class ContactsAppController {
         }
     }
     public void onAddButtonPressed(javafx.event.ActionEvent actionEvent) {
+        firstNameTextField.clear();
+        lastNameTextField.clear();
+        phoneNumberTextField.clear();
+        emailTextField.clear();
+        addButton.setDisable(true);
+        saveButton.setDisable(false);
+    }
+    public void onSaveButtonPressed(javafx.event.ActionEvent actionEvent){
+        Contacts newContact = new Contacts(" "," "," "," ");
+        newContact.setFirst(firstNameTextField.getText());
+        newContact.setLast(lastNameTextField.getText());
+        newContact.setPhoneNumber(phoneNumberTextField.getText());
+        newContact.setEmail(emailTextField.getText());
+        if(newContact.getFirst() != " " && newContact.getLast() != " " && newContact.getPhoneNumber() != " " && newContact.getEmail() != " "){
+            contacts.add(newContact);
+            contactsListView.setItems(contacts); 
+        }
 
-
+        addButton.setDisable(false);
+        saveButton.setDisable(true);
     }
 
 }
